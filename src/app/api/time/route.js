ae9c92484cdd08444538a0f43fc28271ea34cff0
Fazt <fazt@faztweb.com>
2024-01-22 22:21:00 -0500

    request added

--- src/app/api/time/route.js
+++ src/app/api/time/route.js
@@ -1,7 +1,7 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
   const result = await prisma.$queryRaw`SELECT NOW()`;
   return NextResponse.json({
     result,
